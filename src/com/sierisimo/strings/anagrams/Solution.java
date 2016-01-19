239688e7ae881fd12ea7c8f4d108755810ae79a3
Sier Violencia <sierisimo@gmail.com>
2016-01-19 12:09:34 -0600

    Anagrams ready

--- src/com/sierisimo/strings/anagrams/Solution.java
+++ src/com/sierisimo/strings/anagrams/Solution.java
@@ -1,57 +1,39 @@
 package com.sierisimo.strings.anagrams;
 
-import com.sierisimo.common.MergeClass;
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Created by sierisimo on 17/01/16.
  */
 public class Solution {
-    private static String sortLexicographic(String arg) {
-        char[] sorted = mergeSort(arg);
-        System.out.println(new String(sorted));
-        return "";
-    }
-
-    private static char[] mergeSort(String arg) {
-        System.out.println("Starting mergeSort with: " + arg);
-
-        if (arg.length() == 1) return arg.toCharArray();
-
-        char[] result = new char[arg.length()];
-
-        if (arg.length() == 2) {
-            char[] chars = arg.toCharArray();
-            char tmp;
-
-            if (chars[0] > chars[1]) {
-                tmp = chars[0];
-                chars[1] = chars[0];
-                chars[0] = tmp;
-            }
-
-            return chars;
-        } else {
-            char[] hold;
-            for (int i = 0; i < result.length; i++){
-//                hold =
-            }
-        }
-
-        return result;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-//        String a = scanner.next();
-//        String b = scanner.next();
-//
-//        sortLexicographic(a);
-        int[] sorted = MergeClass.mergeSort(new int[]{10,9,1,5,7,3,8,4,3,-1});
-        for (int i = 0; i < sorted.length; i++){
-            System.out.println(sorted[i]);
+        String a = scanner.next();
+        String b = scanner.next();
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        char[] a_arr = a.toCharArray();
+        char[] b_arr = b.toCharArray();
+
+        if (a_arr.length != b_arr.length) {
+            System.out.println("Not Anagrams");
+            return;
+        }
+
+        Arrays.sort(a_arr);
+        Arrays.sort(b_arr);
+
+        a = new String(a_arr);
+        b = new String(b_arr);
+
+        if (a.equals(b)) {
+            System.out.println("Anagrams");
+        } else {
+            System.out.println("Not Anagrams");
         }
     }
 }
---
+++ test/MergeSortTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author Created by sierisimo on 18/01/16. Originally for java
+ */
+public class MergeSortTest {
+    //        int[] sorted = MergeClass.mergeSort(new int[]{10,9,1,5,7,3,8,4,3,-1});
+//        int[] sorted = MergeClass.mergeSort(new int[]{10,9,1});//,5,7,3,8,4,3,-1});
+//        for (int i = 0; i < sorted.length; i++){
+//            System.out.println(sorted[i]);
+//        }
+}
